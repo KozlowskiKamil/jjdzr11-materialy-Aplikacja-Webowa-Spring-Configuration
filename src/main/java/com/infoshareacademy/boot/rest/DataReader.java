94b9b59e36eb230984a2c078cda36f6fd56fd4f0
wojciechbulawa <bulawa.wojciech.firma@gmail.com>
2021-05-14 22:28:49 +0200

    Add predefined books mappings

--- src/main/java/com/infoshareacademy/boot/rest/DataReader.java
+++ src/main/java/com/infoshareacademy/boot/rest/DataReader.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.boot.rest;
 
+import com.infoshareacademy.boot.dto.BookDto;
 import com.infoshareacademy.boot.dto.ProductDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 public class DataReader {
 
     private List<ProductDto> products = new LinkedList<>();
+    private List<BookDto> books = new LinkedList<>();
 
     public void setProducts(List<ProductDto> products) {
         this.products = products;
@@ -22,8 +24,21 @@ public class DataReader {
         return products;
     }
 
+    public List<BookDto> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<BookDto> books) {
+        this.books = books;
+    }
+
     @GetMapping("/products")
     public List<ProductDto> show() {
         return products;
+    }
+
+    @GetMapping("/books")
+    public List<BookDto> showBooks() {
+        return books;
     }
 }
